b3f2badc76bafa680b96ffe6f2e83c27c7c07d5b
Guido Morffi <guidotele@gmail.com>
2023-08-23 14:17:02 -0400

    Minor fix

--- echobot.py
+++ echobot.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 app = Client("my_bot")
 
 
-@app.on_message(filters.text & filters.private)
+@app.on_message(filters.private)
 async def echo(client, message):
-    await message.reply("Hola este bot está en desarrollo aún")
+    await app.send_message(message.chat.id,f'Hola {message.from_user.first_name} este bot está en desarrollo aún')
 
 
 app.run()  # Automatically start() and idle()
